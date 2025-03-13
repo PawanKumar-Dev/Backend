45ed1a968547c447dec67a978887da7282ca6618
PawanKumar-Dev <pk687376@gmail.com>
2025-03-13 16:18:07 +0530

    Update

--- 03_modelling/models/todos/subtodo.models.js
+++ 03_modelling/models/todos/subtodo.models.js
@@ -1,5 +1,18 @@
 import mongoose from 'mongoose'
 
-const subtodoSchema = new mongoose.Schema({})
+const subtodoSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required : true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    createdBy : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+})
 
 export const SubTodo = mongoose.model("SubTodo", subtodoSchema)
