da1ce92797a5f95992e453bf48ecf1253ae8af60
PawanKumar-Dev <pk687376@gmail.com>
2025-03-15 10:56:22 +0530

    Update

--- 04_ecommerce_model/models/ecommerce/order.models.js
+++ 04_ecommerce_model/models/ecommerce/order.models.js
@@ -1,5 +1,32 @@
 import mongoose from 'mongoose'
 
-const orderSchema = new mongoose.Schema({}, { timestamps: true })
+const orderItemsSchema = new mongoose.Schema({
+    productID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+
+const orderSchema = new mongoose.Schema({
+    orderPrice: {
+        type: Number,
+        required: true
+    },
+    costumer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    orderItems: {
+        type: [orderItemsSchema]
+    },
+    address: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
 
 export const Order = mongoose.model("Order", orderSchema)
--- 04_ecommerce_model/models/ecommerce/product.models.js
+++ 04_ecommerce_model/models/ecommerce/product.models.js
@@ -19,7 +19,12 @@ const productSchema = new mongoose.Schema({
     },
     category: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Category"
+        ref: "Category",
+        required: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
     },
     productImage: {
         type: String
