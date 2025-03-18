2b1caa8461f41da3fc5d4801d234fe51bf1b9be1
PawanKumar-Dev <pk687376@gmail.com>
2025-03-18 11:13:02 +0530

    Update

---
+++ 05_project/src/utils/cloudinary.js
@@ -0,0 +1,8 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name: 'docnypesr',
+    api_key: '119637745245243',
+    api_secret: '<your_api_secret>'
+})
