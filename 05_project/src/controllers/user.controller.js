90c80bc43a6b9704796576895033c2258fce7e12
PawanKumar-Dev <pk687376@gmail.com>
2025-03-19 18:05:31 +0530

    Update

--- 05_project/src/controllers/user.controller.js
+++ 05_project/src/controllers/user.controller.js
@@ -144,7 +144,25 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // Logout Function
 const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: { refreshToken: undefined }
+        },
+        {
+            new: true
+        }
+    )
 
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+        .clearCookie(accessToken, options)
+        .clearCookie(refreshToken, options)
+        .json(new ApiRespnse(200, {}, "User Loggged Out!"))
 })
 
 export { registerUser, loginUser, logoutUser }
