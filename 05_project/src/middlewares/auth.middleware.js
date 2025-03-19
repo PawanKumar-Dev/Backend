c21e1418001e65a79598e3077adeac6032e6cad7
PawanKumar-Dev <pk687376@gmail.com>
2025-03-19 17:50:37 +0530

    Update

--- 05_project/src/middlewares/auth.middleware.js
+++ 05_project/src/middlewares/auth.middleware.js
@@ -1,7 +1,22 @@
+import ApiError from "../utils/ApiError"
 import asyncHandler from "../utils/asyncHandler"
+import jwt from 'jsonwebtoken'
+import { User } from "../models/user.models"
 
 const verifyJWT = asyncHandler(async (req, res, next) => {
-    
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+    if (!token) {
+        throw new ApiError(401, "Unotherzied Request")
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+    if (!user) {
+        throw new ApiError(401, "Invalid Access Token")
+    }
 })
 
 export default verifyJWT
