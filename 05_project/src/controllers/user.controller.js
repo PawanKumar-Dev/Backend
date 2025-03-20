a93976e730bb31751e4cadef0fad8779139789d6
PawanKumar-Dev <pk687376@gmail.com>
2025-03-20 12:40:53 +0530

    Update

--- 05_project/src/controllers/user.controller.js
+++ 05_project/src/controllers/user.controller.js
@@ -216,4 +216,4 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 })
 
 
-export { registerUser, loginUser, logoutUser }
+export { registerUser, loginUser, logoutUser, refreshAccessToken }
--- 05_project/src/routes/user.routes.js
+++ 05_project/src/routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import { logoutUser, registerUser, loginUser } from '../controllers/user.controller.js'
+import { registerUser, loginUser, logoutUser, refreshAccessToken } from '../controllers/user.controller.js'
 import { upload } from '../middlewares/multer.middlerware.js'
 import verifyJWT from '../middlewares/auth.middleware.js'
 
 const router = Router()
 
+// User Register Route
+// "upload.fields" is middleware from multer allowing us to pass images or multi-part data
 router.route("/register").post(
     upload.fields([
         {
@@ -19,8 +21,18 @@ router.route("/register").post(
     registerUser
 )
 
+// User Login Route
+// "upload.none()" from multer middleware allow us to send data in form method and json as well
 router.route("/login").post(upload.none(), loginUser)
 
+
+// Secured Routes
+// Logout Route which use verifyJWT middleware
 router.route("/logout").post(verifyJWT, logoutUser)
+
+
+// Route to hit when refreshing Access Token
+router.route("/refreshtoken").post(refreshAccessToken)
+
 
 export default router
