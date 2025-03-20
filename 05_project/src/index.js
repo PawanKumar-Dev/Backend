3fb365294353b7f71778e97ba692877060c923f5
PawanKumar-Dev <pk687376@gmail.com>
2025-03-20 13:09:50 +0530

    Update

--- 05_project/src/index.js
+++ 05_project/src/index.js
@@ -3,6 +3,13 @@ import app from './app.js'
 import connectDB from "./db/index.js";
 const port = process.env.PORT || 8000
 
+// "dotenv/config" package must be imported first
+// So we can access our .env variables throughout our App.
+
+
+// Imported DB connection
+// Then started listening at the port
+
 connectDB()
     .then(() => {
         app.listen(port, () => {
