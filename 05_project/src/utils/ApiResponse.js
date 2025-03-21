1fb09dc556c33ac43b5a8034fbbf53d1e78cd09d
PawanKumar-Dev <pk687376@gmail.com>
2025-03-21 11:47:21 +0530

    Update

--- 05_project/src/utils/ApiResponse.js
+++ 05_project/src/utils/ApiResponse.js
@@ -1,3 +1,7 @@
+// Class designed to structure the responses API sends back to clients in standardized-consistent format.
+// "statusCode" : represents HTTP status code of response
+// "data": contains the payload of response
+// "message": provides a human-readable message about API call.
 class ApiResponse {
     constructor(statusCode, data, message = "Success") {
         this.statusCode = statusCode
